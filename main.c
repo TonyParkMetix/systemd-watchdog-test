4b0b1fb6b746b55ad0bc86c4111adb358d9294d9
Tony Park <tony.park@metixmedical.co.uk>
2021-06-18 14:10:43 +0100

    Returned to sd-daemon interface

--- main.c
+++ main.c
@@ -1,28 +1,16 @@
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <sys/ioctl.h>
-#include <linux/types.h>
-#include <linux/watchdog.h>
-
-int fd;
+#include <systemd/sd-daemon.h>
 
 int main(int argc, char ** argv)
 {
 	printf("systemd watchdog test\n");
+	sd_notify(0, "READY=1\n"
+		     "STATUS=main loop running...\n");
 
-    fd = open("/dev/watchdog", O_WRONLY);
-    
-    int dummy;
-    
 	while(1) {
 		sleep(1);
 		printf("kick dog\n");
-        ioctl(fd, WDIOC_KEEPALIVE, &dummy);
+		sd_notify(0, "WATCHDOG=1");
 	}
 }
-
